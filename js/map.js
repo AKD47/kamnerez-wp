d09f3703a18bb7210210bd057beb0350cd67d5ea
AKD47 <van83horn@gmail.com>
2018-01-15 17:15:08 +0300

    map fix

--- js/map.js
+++ js/map.js
@@ -1,89 +1,38 @@
-function Map(){
-
-    this.init = function (options) {
+function Map() {
+    this.init = function (t) {
         this.defaultParams = {
-            selector:'#map',
+            selector: "#map",
             zoom: 7,
             center: [55.76, 37.64],
-            placemarks:[]
-        };
-
-        this.finalParams = this.defaultParams;
-
-        for (var key in options) {
-            if (options.hasOwnProperty(key)) {
-                if (options[key] !== undefined) {
-                    this.finalParams[key] = options[key];
-                }
-            }
-        }
-        this.options = this.finalParams;
-
-        ymaps.ready(this.run.bind(this));
-
-    }
-
-    this.run = function(){
-
-        if(typeof this.options.center === 'string'){
-            var result = ymaps.geocode(this.options.center);
-            result.done(function (res) {
+            placemarks: []
+        }, this.finalParams = this.defaultParams;
+        for (var e in t)t.hasOwnProperty(e) && void 0 !== t[e] && (this.finalParams[e] = t[e]);
+        this.options = this.finalParams, ymaps.ready(this.run.bind(this))
+    }, this.run = function () {
+        if ("string" == typeof this.options.center) {
+            ymaps.geocode(this.options.center).done(function (t) {
                 this.map = new ymaps.Map(this.getMapBox(), {
-                    center: [res.geoObjects.get(0).geometry.getCoordinates()[0],res.geoObjects.get(0).geometry.getCoordinates()[1]],
+                    center: [t.geoObjects.get(0).geometry.getCoordinates()[0], t.geoObjects.get(0).geometry.getCoordinates()[1]],
                     zoom: this.options.zoom
-                });
-                this.printPlacemarks();
+                }), this.printPlacemarks()
             }.bind(this), function () {
-                console.log('Произошла ошибка.');
-            });
-        }
-        else{
-            this.map = new ymaps.Map(this.getMapBox(), {
-                center: this.options.center,
-                zoom: this.options.zoom
-            });
-        }
-
-
+                console.log("РџСЂРѕРёР·РѕС€Р»Р° РѕС€РёР±РєР°.")
+            })
+        } else this.map = new ymaps.Map(this.getMapBox(), {center: this.options.center, zoom: this.options.zoom})
+    }, this.printPlacemarks = function () {
+        this.options.placemarks.forEach(function (t) {
+            this.addPlacemark(t.address, t.properties, t.options)
+        }.bind(this))
+    }, this.addPlacemark = function (t, e, o) {
+        var s = ymaps.geoQuery(ymaps.geocode(t)).slice(0, 1);
+        s.then(function () {
+            o && o.forEach(function (t) {
+                s.setOptions(t.key, t.value)
+            }), e && e.forEach(function (t) {
+                s.setProperties(t.key, t.value)
+            }), s.addToMap(this.map)
+        }.bind(this))
+    }, this.getMapBox = function () {
+        return "object" == typeof this.options.selector ? this.options.selector : "#" == this.options.selector[0] ? document.getElementById(this.options.selector.slice(1)) : document.getElementsByClassName(this.options.selector.slice(1))
     }
-
-    this.printPlacemarks = function(){
-        this.options.placemarks.forEach(function(a){
-            this.addPlacemark(a.address, a.properties, a.options);
-        }.bind(this));
-    }
-
-    this.addPlacemark = function(address, properties, options){
-        var r = ymaps.geoQuery(ymaps.geocode(address)).slice(0,1);
-        r.then(function(){
-            if(options){
-                options.forEach(function(opt){
-                    r.setOptions(opt.key, opt.value);
-                });
-            }
-            if(properties){
-                properties.forEach(function(prop){
-                    r.setProperties(prop.key, prop.value);
-                });
-            }
-            r.addToMap(this.map);
-        }.bind(this));
-    }
-
-    this.getMapBox = function(){
-        var mapBox;
-        if (typeof this.options.selector == "object") {
-            mapBox = this.options.selector;
-        }
-        else {
-            if (this.options.selector[0] == '#') {
-                mapBox = document.getElementById(this.options.selector.slice(1));
-            }
-            else {
-                mapBox = document.getElementsByClassName(this.options.selector.slice(1));
-            }
-        }
-        return mapBox;
-    }
-
 }
